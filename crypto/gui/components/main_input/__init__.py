3f6855d657acc11fd53ab4a35255e75ffd873e91
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-09-03 00:57:37 +0700

    Create tabwidget component

---
+++ crypto/gui/components/main_input/__init__.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import QGroupBox, QWidget, QVBoxLayout, QTabWidget, QSizePolicy
+from PyQt5.QtCore import QSize
+
+from crypto.gui.components.main_input.tab_file import TabFile
+from crypto.gui.components.main_input.tab_string import TabString
+
+
+class MainInput(QTabWidget):
+    def __init__(self, size: QSize, parent: QWidget = None):
+        super(MainInput, self).__init__(parent=parent)
+        self.setupUi()
+
+    def setupUi(self):
+        self.tab_string = TabString()
+        self.tab_file = TabFile()
+        self.addTab(self.tab_string, 'String Input')
+        self.addTab(self.tab_file, 'File Input')
+        self.setCurrentIndex(0)
