ed8e635bb22ba2348247713fd4cfc215fab240d6
jrandiny <jrandiny@gmail.com>
2020-09-02 22:28:32 +0700

    Add helper for input check on base_engine

--- crypto/engine/base_engine.py
+++ crypto/engine/base_engine.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from crypto.engine.data import Data
-from crypto.engine.key import KeyType, Key
+from crypto.engine.data import *
+from crypto.engine.key import *
 
 
 @dataclass
@@ -15,6 +15,18 @@ class EngineCapabilities:
 class BaseEngine(ABC):
     def __init__(self, engine_capabilities: EngineCapabilities):
         self.capabilities = engine_capabilities
+
+    def _check_data_supported(self, data: Data):
+        if data.data_type == DataType.FILE and not self.capabilities.support_file:
+            raise Exception('Engine doesn\'t support file input')
+        if data.data_type == DataType.TEXT and not self.capabilities.support_text:
+            raise Exception('Engine doesn\'t support text input')
+
+    def _check_key_format(self, key: Key):
+        if key.key_type != self.capabilities.key_type:
+            raise Exception('Invalid key type')
+        if len(key.data) != self.capabilities.key_length:
+            raise Exception('Invalid key length')
 
     @abstractmethod
     def encrypt(self, data: Data, key: Key) -> Data:
