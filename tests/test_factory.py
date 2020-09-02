ea63476ff3b6f039ac49ffc3f8de0c7e120bee16
jrandiny <jrandiny@gmail.com>
2020-09-02 22:12:43 +0700

    Add simple factory test

---
+++ tests/test_factory.py
@@ -0,0 +1,12 @@
+from crypto.engine.engine_factory import *
+from crypto.engine.vigenere_engine import VigenereEngine
+
+
+def test_list_engine():
+    engine_list = EngineType.list()
+    assert len(engine_list) == 1
+
+
+def test_create_engine():
+    vigenere = EngineFactory.create_engine(EngineType.VIGENERE)
+    assert type(vigenere) == VigenereEngine
