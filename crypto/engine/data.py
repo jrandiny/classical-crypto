c1dbec2d7ee9f17d40bf94b72a2a6de2f51eed19
jrandiny <jrandiny@gmail.com>
2020-09-02 22:08:15 +0700

    Add missing typehint

--- crypto/engine/data.py
+++ crypto/engine/data.py
@@ -6,17 +6,17 @@ class DataType(Enum):
     FILE = 'file'
     TEXT = 'text'
 
+
 class Data:
     def __init__(self, data_type: DataType, data: str):
         self.data_type = data_type
         self._data = data
 
-    def get_text()->str:
+    def get_text() -> str:
         assert self.data_type == DataType.TEXT
         return self._data
 
-    def get_file()->:
+    def get_file() -> TextIO:
         assert self.data_type == DataType.FILE
         abc = open('asdas', 'r')
         return self._data
-
