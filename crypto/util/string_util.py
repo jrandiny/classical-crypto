94058b7381eb59c7d38de007effedc578e451977
jrandiny <jrandiny@gmail.com>
2020-09-03 00:30:10 +0700

    Add random generator string util

--- crypto/util/string_util.py
+++ crypto/util/string_util.py
@@ -1,5 +1,7 @@
 import textwrap
 import re
+import random
+import string
 
 
 class StringUtil:
@@ -15,3 +17,7 @@ class StringUtil:
     def split_to_group(input: str) -> str:
         input_no_space = StringUtil.remove_space(input)
         return ' '.join(textwrap.wrap(input_no_space, width=5))
+
+    @staticmethod
+    def generate_random_string(length: int) -> str:
+        return ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
