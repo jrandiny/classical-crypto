75f65b81a05f49490bf4f5dac5a82d5a7a2adb76
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-09-03 08:47:02 +0700

    Add layout to main window

--- crypto/gui/main_window.py
+++ crypto/gui/main_window.py
@@ -1,13 +1,10 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QHBoxLayout
 from PyQt5.QtCore import QSize
 
 from crypto.gui.components.algorithm_list import AlgorithmList
 from crypto.gui.components.main_input import MainInput
 from crypto.gui.components.configuration import Configuration
-
-from crypto.gui.components.main_input.tab_file import TabFile
-from crypto.gui.components.main_input.tab_string import TabString
 
 list_of_algorithm = [
     'Playfair Cipher', 'Auto-key Vigenere Cipher', 'Full Vigenere Cipher',
@@ -27,17 +24,22 @@ class MainWindow(QMainWindow):
         self.central_widget = QtWidgets.QWidget(self)
         self.central_widget.setObjectName("central_widget")
 
+        self.layout = QHBoxLayout()
+
         self.algorithm_list = AlgorithmList('Algorithm', list_of_algorithm,
                                             QSize(241, 512),
                                             self.central_widget)
-        self.algorithm_list.move(10, 20)
 
         self.main_input = MainInput(QSize(361, 531), self.central_widget)
-        self.main_input.move(270, 10)
 
         self.configuration = Configuration('Configuration', QSize(221, 521),
                                            self.central_widget)
-        self.configuration.move(640, 20)
+
+        self.layout.addWidget(self.algorithm_list)
+        self.layout.addWidget(self.main_input)
+        self.layout.addWidget(self.configuration)
+
+        self.central_widget.setLayout(self.layout)
 
         self.setCentralWidget(self.central_widget)
         self.statusbar = QtWidgets.QStatusBar(self)
