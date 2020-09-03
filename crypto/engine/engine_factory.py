ebb99c4fdd834aaaf98fe86e8b46e012afc0b4c8
jrandiny <jrandiny@gmail.com>
2020-09-03 22:47:19 +0700

    Add full vigenere and playfair to factory

--- crypto/engine/engine_factory.py
+++ crypto/engine/engine_factory.py
@@ -1,12 +1,16 @@
 from crypto.engine.base_engine import BaseEngine
-from crypto.engine.vigenere_engine import VigenereEngine
 from crypto.engine.autokey_vigenere_engine import AutokeyVigenereEngine
+from crypto.engine.full_vigenere_cipher import FullVigenereEngine
+from crypto.engine.vigenere_engine import VigenereEngine
+from crypto.engine.playfair_engine import PlayfairEngine
 from enum import Enum
 
 
 class EngineType(Enum):
     VIGENERE = 'vigenere'
     VIGENERE_AUTOKEY = 'vigenere_autokey'
+    VIGENERE_FULL = 'vigenere_full'
+    PLAYFAIR = 'playfair'
 
     @staticmethod
     def list():
@@ -20,5 +24,9 @@ class EngineFactory():
             return VigenereEngine()
         elif engine_type == EngineType.VIGENERE_AUTOKEY:
             return AutokeyVigenereEngine()
+        elif engine_type == EngineType.VIGENERE_FULL:
+            return FullVigenereEngine()
+        elif engine_type == EngineType.PLAYFAIR:
+            return PlayfairEngine()
         else:
             raise Exception('Unsupported engine')
