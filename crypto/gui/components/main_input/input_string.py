11737b28c979b40167a7576bb5cd1b3f55f7ddbc
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-09-04 16:46:25 +0700

    Add copy to clipboard method

--- crypto/gui/components/main_input/input_string.py
+++ crypto/gui/components/main_input/input_string.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QTextEdit, QVBoxLayout
 from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QClipboard, QGuiApplication
 
 
 class InputString(QWidget):
@@ -14,5 +15,12 @@ class InputString(QWidget):
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.text_edit)
         self.layout.addWidget(self.copy_button)
-
         self.setLayout(self.layout)
+
+        self.copy_button.clicked.connect(self.copy_to_clipboard)
+
+    def copy_to_clipboard(self):
+        text = self.text_edit.toPlainText()
+        clip_board = QGuiApplication.clipboard()
+        clip_board.clear(mode=clip_board.Clipboard)
+        clip_board.setText(text, mode=clip_board.Clipboard)
