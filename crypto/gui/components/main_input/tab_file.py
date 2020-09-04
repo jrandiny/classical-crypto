b525f26e8f5fc24d94a59e8586a1f1c75a368949
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-09-04 11:07:00 +0700

    Change toggle button mechanism

--- crypto/gui/components/main_input/tab_file.py
+++ crypto/gui/components/main_input/tab_file.py
@@ -1,7 +1,6 @@
-from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel
 
 from crypto.gui.components.main_input.input_file import InputFile
-from crypto.gui.components.main_input.toggle_button import ToggleButton
 
 
 class TabFile(QWidget):
@@ -13,13 +12,20 @@ class TabFile(QWidget):
         self.input = InputFile('Import')
         self.output = InputFile('Browse')
 
-        self.mode_toggle = ToggleButton(['Encrypt', 'Decrypt'])
+        self.btn_switch = QPushButton('Switch Mode')
         self.btn_execute = QPushButton('Execute')
+        self.lbl_mode_title = QLabel('Mode:')
+        self.lbl_mode_value = QLabel('Encrypt')
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.input)
-        self.layout.addWidget(self.output)
-        self.layout.addWidget(self.mode_toggle)
-        self.layout.addWidget(self.btn_execute)
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget(self.lbl_mode_title)
+        self.h_layout.addWidget(self.lbl_mode_value)
 
-        self.setLayout(self.layout)
+        self.v_layout = QVBoxLayout()
+        self.v_layout.addWidget(self.input)
+        self.v_layout.addWidget(self.output)
+        self.v_layout.addLayout(self.h_layout)
+        self.v_layout.addWidget(self.btn_switch)
+        self.v_layout.addWidget(self.btn_execute)
+
+        self.setLayout(self.v_layout)
--- crypto/gui/components/main_input/tab_string.py
+++ crypto/gui/components/main_input/tab_string.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel
+from PyQt5.QtCore import Qt
 
 from crypto.gui.components.main_input.input_string import InputString
-from crypto.gui.components.main_input.toggle_button import ToggleButton
 
 
 class TabString(QWidget):
@@ -12,15 +12,21 @@ class TabString(QWidget):
     def setupUi(self):
         self.input = InputString()
         self.output = InputString()
-        self.mode_toggle = ToggleButton(['Encrypt', 'Decrypt'])
+        self.btn_switch = QPushButton('Switch Mode')
         self.btn_execute = QPushButton('Execute')
+        self.lbl_mode_title = QLabel('Mode:')
+        self.lbl_mode_value = QLabel('Encrypt')
 
-        self.h_layout = QHBoxLayout()
-        self.h_layout.addWidget(self.mode_toggle)
-        self.h_layout.addWidget(self.btn_execute)
+        self.h_layout_1 = QHBoxLayout()
+        self.h_layout_2 = QHBoxLayout()
+        self.h_layout_1.addWidget(self.lbl_mode_title)
+        self.h_layout_1.addWidget(self.lbl_mode_value)
+        self.h_layout_2.addWidget(self.btn_switch)
+        self.h_layout_2.addWidget(self.btn_execute)
         self.v_layout = QVBoxLayout()
         self.v_layout.addWidget(self.input)
-        self.v_layout.addLayout(self.h_layout)
+        self.v_layout.addLayout(self.h_layout_1)
+        self.v_layout.addLayout(self.h_layout_2)
         self.v_layout.addWidget(self.output)
 
         self.setLayout(self.v_layout)
