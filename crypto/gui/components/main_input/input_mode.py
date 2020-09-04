10fc21dc7db1a99efc0097d3b0bf343b7176522c
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-09-04 16:03:38 +0700

    Add signal to toggle encrypt/decrypt

--- crypto/gui/components/main_input/input_mode.py
+++ crypto/gui/components/main_input/input_mode.py
@@ -1,19 +1,24 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QSpacerItem, QSizePolicy
 from PyQt5.QtCore import Qt
 
+from crypto.gui.encryption_parameter import ModeType
+
 
 class InputMode(QWidget):
     def __init__(self, parent=None):
         super(InputMode, self).__init__(parent)
+        self.is_encrypt = False
         self.setupUi()
 
     def setupUi(self):
         self.btn_switch = QPushButton('Switch Mode')
         self.btn_execute = QPushButton('Execute')
-        self.lbl_mode_title = QLabel('Mode:')
+        self.lbl_mode_title = QLabel('Mode')
+        self.lbl_colon = QLabel(':')
         self.lbl_mode_value = QLabel('Encrypt')
 
         self.lbl_mode_title.setAlignment(Qt.AlignRight)
+        self.lbl_colon.setAlignment(Qt.AlignCenter)
         self.lbl_mode_value.setAlignment(Qt.AlignLeft)
 
         self.spacer = QSpacerItem(20, 10, QSizePolicy.Fixed,
@@ -24,7 +29,7 @@ class InputMode(QWidget):
         self.v_layout = QVBoxLayout()
 
         self.h_layout_1.addWidget(self.lbl_mode_title)
-        self.h_layout_1.addSpacerItem(self.spacer)
+        self.h_layout_1.addWidget(self.lbl_colon)
         self.h_layout_1.addWidget(self.lbl_mode_value)
         self.h_layout_2.addWidget(self.btn_switch)
         self.h_layout_2.addWidget(self.btn_execute)
@@ -33,3 +38,13 @@ class InputMode(QWidget):
         self.v_layout.addLayout(self.h_layout_2)
 
         self.setLayout(self.v_layout)
+
+        self.btn_switch.clicked.connect(self.switch_mode)
+        self.btn_switch.animateClick()
+
+    def switch_mode(self):
+        self.is_encrypt = not self.is_encrypt
+        if self.is_encrypt:
+            self.lbl_mode_value.setText(ModeType.ENCRYPT.value)
+        else:
+            self.lbl_mode_value.setText(ModeType.DECRYPT.value)
