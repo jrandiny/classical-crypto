0b5f8a0df5bd5416d317d6e14950d267e1b01a40
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-09-07 21:57:03 +0700

    Connect ui to engine to process file

--- crypto/gui/main_window.py
+++ crypto/gui/main_window.py
@@ -9,6 +9,8 @@ from crypto.gui.dialog_window import DialogWindow
 from crypto.gui.encryption_parms import EncryptionParms, ModeType
 from crypto.gui.worker import Worker
 from crypto.engine.engine_factory import EngineType
+from crypto.engine.data import *
+from crypto.util.file_util import FileUtil
 
 from multiprocessing import cpu_count
 
@@ -40,7 +42,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.central_widget)
 
         self.main_input.tab_string.input_mode.btn_execute.clicked.connect(self.execute_string)
-        self.main_input.tab_file.input_mode.btn_execute.clicked.connect(self.execute_string)
+        self.main_input.tab_file.input_mode.btn_execute.clicked.connect(self.execute_file)
 
         output_conf_signal = self.configuration_box.post_processing.btn_group.idClicked
         output_conf_slot = self.main_input.tab_string.output_string.on_change_format
@@ -50,22 +52,23 @@ class MainWindow(QMainWindow):
         engine_type_slot = self.configuration_box.encryption_box.on_update_key_widget
         engine_type_signal.connect(engine_type_slot)
 
-    def show_error_dialog(self, error_msg):
-        DialogWindow('Error', error_msg).exec_()
+    def show_dialog_window(self, title, msg):
+        DialogWindow(title, msg).exec_()
 
-    def execute_string(self):
+    def show_error_dialog(self, error_msg):
+        self.show_dialog_window('Error', error_msg)
+
+    def show_success_window(self, msg):
+        self.show_dialog_window('Success', msg)
+
+    def prepare_exec_fun(self):
         mode = self.enc_parms.mode
         engine = self.enc_parms.get_engine()
 
-        try:
-            key = self.configuration_box.encryption_box.get_key()
-            data = self.main_input.tab_string.input_string.get_data()
-        except Exception as e:
-            self.show_error_dialog(str(e))
-            return
+        key = self.configuration_box.encryption_box.get_key()
+        data = self.main_input.get_data()
 
         key = engine.complete_key(data, key)
-
         self.configuration_box.encryption_box.apply_key(key)
 
         if mode == ModeType.ENCRYPT:
@@ -73,10 +76,30 @@ class MainWindow(QMainWindow):
         else:
             fn = engine.decrypt
 
+        return fn, key, data
+
+    def execute_string(self):
+        try:
+            fn, key, data = self.prepare_exec_fun()
+        except Exception as e:
+            self.show_error_dialog(str(e))
+            return
+
         worker = Worker(fn, data, key)
         worker.signals.error.connect(self.show_error_dialog)
         worker.signals.result.connect(self.main_input.tab_string.output_string.on_result_update)
         QThreadPool.globalInstance().start(worker)
 
     def execute_file(self):
-        pass
+        try:
+            fn, key, data = self.prepare_exec_fun()
+        except Exception as e:
+            self.show_error_dialog(str(e))
+            return
+
+        file_fn = FileUtil.with_move_file(fn, self.main_input.get_output_path())
+
+        worker = Worker(file_fn, data, key)
+        worker.signals.error.connect(self.show_error_dialog)
+        worker.signals.result.connect(self.show_success_window)
+        QThreadPool.globalInstance().start(worker)
